888c7be42c667d25c3c784fe58850e8cc5cdf220
Marta Roldan <mrmartaroldan@gmail.com>
2017-12-12 19:28:11 +0000

    Bug fixed

--- src/sections/Screen2.js
+++ src/sections/Screen2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text} from 'react-native';';
+import { View, StyleSheet, Text} from 'react-native';
 
 export default class Screen1 extends Component {
 
